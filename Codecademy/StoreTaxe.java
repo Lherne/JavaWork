010180ef912a906fd1f4b5c3ed766e458cf05cbc
Lherne <lherne@gmail.com>
2020-12-10 16:02:25 +0100

    New storeTaxe

---
+++ Codecademy/StoreTaxe.java
@@ -0,0 +1,31 @@
+public class StoreTaxe {
+    // instance fields
+    String productType;
+    double price;
+    
+    // constructor method
+    public StoreTaxe(String product, double initialPrice) {
+      productType = product;
+      price = initialPrice;
+    }
+    
+    // increase price method
+    public void increasePrice(double priceToAdd){
+      double newPrice = price + priceToAdd;
+      price = newPrice;
+    }
+    
+    // get price with tax method
+    //On definis le prix de la taxe et on demande de nous retourner le nouveau prix avec la nouvelle taxe.
+    public double getPriceWithTax() {
+    double totalPrice = price + price * 0.08;
+    return totalPrice;  
+    }
+  
+    // main method
+    public static void main(String[] args) {
+      StoreTaxe lemonadeStand = new Store("Lemonade", 3.75);
+      double lemonadePrice = lemonadeStand.getPriceWithTax();
+      System.out.println(lemonadePrice);
+    }
+  }
